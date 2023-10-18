f0bf7e9eb5b7cb5623d28905c3024589a1328167
Samson Ayeni <ayenisamson8@gmail.com>
2023-10-17 19:13:54 -0700

    get_sigint

---
+++ get_sigint.c
@@ -0,0 +1,12 @@
+#include "main"
+
+/**
+ * get_sigint - To handle the crtl + c call in prompt
+ * @sig: Signal handle
+ */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
+
